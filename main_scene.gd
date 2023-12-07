3da69a76e468fae83bbe174cd15fc127cfe53245
Konstantin Prokopchik <kprokopchik@gmail.com>
2023-12-06 19:52:52 -0800

    Experimenting with tower placeholders
    Found 2 alternatives to determine if player picks a proper place for a tower:
    * Use tile map with custom data layer
    * Use Curve2D from a Path node to find nearest point

--- main_scene.gd
+++ main_scene.gd
@@ -9,20 +9,12 @@ var state: String = PEACE
 const PEACE = "peace"
 const WAR = "war"
 
-const TWR_HLT_ATLAS = Vector2i(6, 7)
-const TWR_PLACEHOLDERS: Array[Vector2i] = [\
-	Vector2i(1, 3), \
-	Vector2i(0, 4), \
-	Vector2i(2, 4), \
-	Vector2i(1, 5)]
-var hover_tile_pos: Vector2i
 var tile_bkp_pos: Vector2i
-var tile_bkp_atlas: Vector2i = Vector2i.MIN
-
 var tower_to_build_id: int = -1
 
 @onready var next_wave_timer: NextWaveTimer = $NextWaveTimer
 @onready var tile_map = $TileMap
+
 
 func _ready():
 	addPlayer(Player.new("a"), Vector2(85, 50), Color(0, 0, 0)).mana = 20
@@ -49,41 +41,56 @@ func _physics_process(delta: float):
 	if state == WAR:
 		spawnMobs()
 
+func if_tile_for_tower() -> Vector2i:
+	var cell: Vector2i = tile_map.local_to_map(tile_map.get_local_mouse_position())
+	var data = tile_map.get_cell_tile_data(0, cell)
+	if data && data.get_custom_data("twr_places"):
+		return cell
+	else:
+		return Vector2i.MIN
+
+
 func _input(event: InputEvent):
-	var mouse_pos = get_global_mouse_position()
-	var pointed_tile_pos = tile_map.local_to_map(mouse_pos)
-	if hover_tile_pos != pointed_tile_pos:
-		#print("Tile: " + str(pointed_tile_pos))
-		if tile_bkp_atlas != Vector2i.MIN:
-			tile_map.set_cell(0, tile_bkp_pos, 0, tile_bkp_atlas)
-			tile_bkp_atlas = Vector2i.MIN
-		hover_tile_pos = pointed_tile_pos
-		var pointed_tile_atlas = tile_map.get_cell_atlas_coords(0, pointed_tile_pos)
-		if TWR_PLACEHOLDERS.has(pointed_tile_atlas):
-			#print("Placeholder: " + str(pointed_tile_atlas))
-			tile_bkp_pos = hover_tile_pos
-			tile_bkp_atlas = tile_map.get_cell_atlas_coords(0, hover_tile_pos)
-			tile_map.set_cell(0, hover_tile_pos, 0, TWR_HLT_ATLAS)
-	if event.is_action_pressed("click"):
-		var pointed_tile_atlas = tile_map.get_cell_atlas_coords(0, pointed_tile_pos)
-		if pointed_tile_atlas == TWR_HLT_ATLAS:
-			print("click on placeholder")
+	
+	# POC for placing towers without tile data
+	#var mouse_pos = get_global_mouse_position()
+	#var p = $Path2D.curve.get_closest_point(mouse_pos)
+	#var p2 = $Path2D2.curve.get_closest_point(mouse_pos)
+	#var d1 = mouse_pos.distance_squared_to(p)
+	#var d2 = mouse_pos.distance_squared_to(p2)
+	#if d1 < d2:
+		#if d1 < 2500:
+			#$Sprite2D.position = p
+			#$Sprite2D.show()
+		#else:
+			#$Sprite2D.hide()
+	#elif d2 < 2500:
+		#$Sprite2D.position = p2
+		#$Sprite2D.show()
+	#else:
+		#$Sprite2D.hide()
+
+	var tile_coord = if_tile_for_tower()
+	if tile_coord != Vector2i.MIN:
+		$TowerPlaceholder.position = tile_map.map_to_local(tile_coord)
+		$TowerPlaceholder.show()
+		if event.is_action_pressed("click"):
+			tile_bkp_pos = tile_coord
 			var builderMenuPrefab = preload("res://build_tower_menu.tscn")
 			var builder_menu = builderMenuPrefab.instantiate()
-			builder_menu.position = mouse_pos
+			builder_menu.position = tile_map.map_to_local(tile_coord)
 			var scr_size = get_viewport().get_visible_rect().size
-			print("Display + " + str(scr_size))
 			if (builder_menu.position.x + builder_menu.size.x) > scr_size.x:
 				builder_menu.position.x -= builder_menu.size.x
 			if (builder_menu.position.y + builder_menu.size.y) > scr_size.y:
 				builder_menu.position.y -= builder_menu.size.y
-				
-
-
 			builder_menu.build_tower_confirmed.connect(on_build_tower_confirmed)
 			builder_menu.build_tower_selected.connect(on_build_tower_selected)
 			add_child(builder_menu)
-			
+	else:
+		$TowerPlaceholder.hide()
+
+
 func on_build_menu_closed():
 	tower_to_build_id = -1
 
